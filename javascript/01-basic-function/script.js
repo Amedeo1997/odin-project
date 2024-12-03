599c59a87e31db42f10de1d1264135b821a379fb
Amedeo1997 <sirjaizz@gmail.com>
2024-12-03 16:02:18 +0100

    normal function done

---
+++ javascript/01-basic-function/script.js
@@ -0,0 +1,23 @@
+function add7(n) {
+    return n + 7;
+}
+
+add7(5);
+
+function multiply(n, i) {
+    return n * i;
+}
+
+multiply(5, 10);
+
+function capitalize(s) {
+    return s.toUpperCase();
+}
+
+capitalize("Hello my friend")
+
+function lastLetter(s) {
+    return s.charAt(s.length - 1);
+}
+
+lastLetter("Hello");
