0ac190643b3f67b6f7886bfd41650c5a875d6115
Amedeo1997 <sirjaizz@gmail.com>
2024-12-03 16:34:50 +0100

    FizzBuzz done

--- javascript/02-fizz-buzz/script.js
+++ javascript/02-fizz-buzz/script.js
@@ -6,3 +6,18 @@ If the current number is divisible by 5 then print "Buzz"
 If the current number is divisible by 3 and 5 then print "FizzBuzz"
 Otherwise print the current number 
 */
+
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "))
+    
+    for (let i = 1; i <= answer; i++) {
+        if (i % 3 === 0 && i % 5 !== 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            console.log("Buzz");
+        } else if (i % 5 === 0 && i % 3 === 0) {
+            console.log("FizzBuzz")
+        } else {
+            console.log(i);
+        }
+    }
