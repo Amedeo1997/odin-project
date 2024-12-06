dfc00f16dcb41f96e8f73ff0c0c3e84eb8f18b48
Amedeo1997 <sirjaizz@gmail.com>
2024-12-06 18:39:43 +0100

    h3 create

--- javascript/03-Dom/script.js
+++ javascript/03-Dom/script.js
@@ -6,4 +6,15 @@ newParagraph.textContent = "Hey I'm red!";
 
 newParagraph.style.color = "red";
 
+// Add h3 with blue text  that says hey I'm blue
+
+const newHash = document.createElement('h3');
+
+newHash.style.color = "blue";
+
+newHash.textContent = "Hey I'm blue";
+
 container.appendChild(newParagraph)
+
+container.appendChild(newHash);
+
